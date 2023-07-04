331198238d702a93a6a8c1dc64efc60559cb743b
Heliel Souza Lima <helielsouza01234@gmail.com>
2023-07-04 16:41:24 -0300

    creating model and migrations to database

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 
 from . import views  # imported by recipes
 
+app_name = 'recipes'
+
 urlpatterns = [
-    path('', views.home),  # function inside recipes app to show home page
-    path('recipes/<id>/', views.recipe)  # to show one recipe
+    # function inside recipes app to show home page
+    path('', views.home, name="home"),
+    # to show one recipe
+    path('recipes/<int:id>/', views.recipe, name="recipe")
+
 ]
