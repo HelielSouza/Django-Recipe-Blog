6a73493a0268eb4b033978eab78de163cc6902b5
Heliel Souza Lima <helielsouza01234@gmail.com>
2023-07-20 14:16:08 -0300

    fixing 404 recipes id error

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,9 +5,8 @@ from . import views  # imported by recipes
 app_name = 'recipes'
 
 urlpatterns = [
-    # function inside recipes app to show home page
     path('', views.home, name="home"),
-    # to show one recipe
-    path('recipes/<int:id>/', views.recipe, name="recipe")
-
+    path('recipes/category/<int:category_id>/',
+         views.category, name="category"),
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
 ]
