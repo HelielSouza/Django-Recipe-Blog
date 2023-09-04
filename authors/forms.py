56ac9914f483f740cb1668e1467cf077d3a57388
Heliel Souza Lima <helielsouza01234@gmail.com>
2023-09-04 20:10:49 -0300

    Including password validations

--- authors/forms.py
+++ authors/forms.py
@@ -1,8 +1,73 @@
+import re
+
 from django import forms
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
+
+# funcao para modificar algum atributo
+def add_attr(field, attr_name, attr_new_val):
+    existing = field.widget.attrs.get(attr_name, '')
+    field.widget.attrs[attr_name] = f'{existing} {attr_new_val}'.strip()
+
+
+# funcao para adicionar placeholder
+def add_placeholder(field, placeholder_val):
+    add_attr(field, 'placeholder', placeholder_val)
+
+
+# criação de senha forte
+def strong_password(password):
+    regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
+
+    if not regex.match(password):
+        raise ValidationError((
+            'A senha deve ter no mínimo uma letra maiúscula, '
+            'Uma letra minúscula e um número. A senha deve '
+            'possuir pelo menos 8 caracteres.'
+        ),
+            code='invalid'
+        )
 
 
 class RegisterForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        add_placeholder(self.fields['username'], 'Digite seu nome de usuário')
+        add_placeholder(self.fields['first_name'], 'Digite seu primeiro nome')
+        add_placeholder(self.fields['last_name'], 'Digite seu sobrenome')
+        add_placeholder(self.fields['email'], 'Digite seu e-mail')
+
+    # senha principal
+    password = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Sua senha'
+        }),
+        error_messages={
+            'required': 'A senha não pode estar vazia'
+        },
+        help_text=(
+            'Senha deve ter pelo menos um caracter maiúsculo, '
+            'um caracter minúsculo e um número. A senha deve '
+            'possuir pelo menos 8 caracteres.'
+        ),
+        label='Digite sua senha',
+        validators=[strong_password]
+    )
+
+    # senha confirmacao
+    password2 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Digite novamente sua senha'
+        }),
+        error_messages={
+            'required': 'A senha não pode estar vazia'
+        },
+        label='Confirme sua senha',
+    )
+
     class Meta:
         model = User
         fields = [
@@ -12,3 +77,57 @@ class RegisterForm(forms.ModelForm):
             'email',
             'password',
         ]
+        labels = {
+            'first_name': 'Nome',
+            'username': 'Nome de usuário',
+            'last_name': 'Sobrenome',
+            'email': 'E-mail',
+        }
+        help_texts = {
+            'email': 'O e-mail deve ser válido.',
+        }
+        error_messages = {
+            'username': {
+                'required': 'Esse campo não pode estar vazio',
+            }
+        }
+
+    # Método para nao adicionar 'atenção' como senha
+    def clean_password(self):
+        data = self.cleaned_data.get('password')
+        if 'atenção' in data:
+            raise ValidationError(
+                'Não digite %(pipoca)s no campo password',
+                code='invalid',
+                params={'pipoca': '"atenção"'}
+            )
+        return data
+
+    # Método para o primeiro nome nao ser John Doe
+    def clean_first_name(self):
+        data = self.cleaned_data.get('first_name')
+        if 'John Doe' in data:
+            raise ValidationError(
+                'Não digite %(value)s no campo first name',
+                code='invalid',
+                params={'value': '"John Doe"'}
+            )
+
+    # metodo para verificar se os campos de senhas sao iguais
+    def clean(self):
+        cleaned_data = super().clean()
+
+        password = cleaned_data.get('password')
+        password2 = cleaned_data.get('password2')
+
+        if password != password2:
+            password_confirmation_error = ValidationError(
+                'As senhas devem ser iguais ',
+                code='invalid'
+            )
+            raise ValidationError({
+                'password': password_confirmation_error,
+                'password2': [
+                    password_confirmation_error,
+                ],
+            })
