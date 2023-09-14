d941185944b19f43dfc078b78efcaa103faf497a
Heliel Souza Lima <helielsouza01234@gmail.com>
2023-09-14 19:36:48 -0300

    Login, forms and integrating partials at project

--- authors/views.py
+++ authors/views.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.contrib.auth import authenticate, login
 from django.http import Http404
 from django.shortcuts import redirect, render
 from django.urls import reverse
@@ -41,16 +42,37 @@ def register_create(request):
 # funcao de login
 
 
-def login(request):
+def login_view(request):
     form = LoginForm()
     return render(request, 'authors/pages/login.html', {
         'form': form,
         'form_action': reverse('authors:login_create')
-
     })
 
 # funcao pos login
 
+# verifica se esta autenticado e loga ele, lancando msg
+
 
 def login_create(request):
-    return render(request, 'authors/pages/login.html')
+    if not request.POST:
+        raise Http404()
+
+    form = LoginForm(request.POST)
+    login_url = reverse('authors:login')
+
+    if form.is_valid():
+        authenticated_user = authenticate(
+            username=form.cleaned_data.get('username', ''),
+            password=form.cleaned_data.get('password', ''),
+        )
+
+        if authenticated_user is not None:
+            messages.success(request, 'Você está logado')
+            login(request, authenticated_user)
+        else:
+            messages.error(request, 'Credenciais inválidas')
+    else:
+        messages.error(request, 'Nome de usuário e senha inválidos')
+
+    return redirect(login_url)
