d9c883d05220f0864d986b919d084fa37e9b9d7d
Heliel Souza Lima <helielsouza01234@gmail.com>
2024-01-09 23:44:26 -0300

    Recipe detail now as CBV

--- recipes/views.py
+++ recipes/views.py
@@ -4,7 +4,7 @@ import os
 from django.db.models import Q
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 from utils.pagination import make_pagination
 
 from recipes.models import Recipe
@@ -105,3 +105,18 @@ def recipe(request, id):
         'recipe': recipe,
         'is_detail_page': True,
     })
+
+
+class RecipeDetail(DetailView):
+    model = Recipe
+    context_object_name = 'recipe'
+    template_name = 'recipes/pages/recipe-view.html'
+
+    def get_context_data(self, *args, **kwargs):
+        ctx = super().get_context_data(*args, **kwargs)
+
+        ctx.update({
+            'is_detail_page': True
+        })
+
+        return ctx
