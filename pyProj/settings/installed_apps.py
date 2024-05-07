a9964787ca5583ac74e6b8fe19e31fa031113294
Seu <helielsouza01234@gmail.com>
2024-05-06 23:24:12 -0300

    Organized settings.py in modules

---
+++ pyProj/settings/installed_apps.py
@@ -0,0 +1,17 @@
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    # our created apps
+    'recipes',
+    'authors',
+    'tag',
+    # django debug-toolbar
+    'debug_toolbar',
+
+]
